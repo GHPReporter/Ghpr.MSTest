08f4297f8898477f29d142416ba63c2204383c5c
Evgeniy Kosjakov <42_@mail.ru>
2016-08-18 18:50:20 +0300

    core added

--- Ghpr.MSTest/Program.cs
+++ Ghpr.MSTest/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ghpr.MSTest
+﻿namespace Ghpr.MSTest
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ Ghpr.MSTest/TrxReader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using Ghpr.Core.Interfaces;
+
+namespace Ghpr.MSTest
+{
+    public class TrxReader
+    {
+        //private static string _outputFolder;
+        //private static string _fileName;
+        private static string _fullPath;
+        private readonly XmlDocument _xml;
+
+        public TrxReader(string fullPath)
+        {
+            _fullPath = fullPath;
+            _xml = new XmlDocument();
+        }
+
+        public TrxReader LoadResult()
+        {
+            if (!File.Exists(_fullPath))
+            {
+                throw new FileNotFoundException("Can't find .trx file!", _fullPath);
+            }
+            _xml.Load(_fullPath);
+            return this;
+        }
+
+        public List<ITestRun> GetTestRuns()
+        {
+            var testRuns = new List<ITestRun>();
+
+            return testRuns;
+        }
+
+
+    }
+}
