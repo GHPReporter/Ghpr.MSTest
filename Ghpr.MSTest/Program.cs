64efb5a4bacd59f871eeead987222c14d26a0441
Evgeniy Kosjakov <42_@mail.ru>
2016-08-19 19:35:00 +0300

    getting test runs from trx

--- Ghpr.MSTest/Program.cs
+++ Ghpr.MSTest/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace Ghpr.MSTest
+﻿using System;
+using System.Linq;
+using Ghpr.Core;
+
+namespace Ghpr.MSTest
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            if (!args.Any()) throw new Exception("No path specified!");
+            var path = args[0];
+            var reporter = new Reporter();
+            var reader = new TrxReader(path);
+
+            reader.GenerateReport(reporter);
 
         }
     }
--- Ghpr.MSTest/TrxReader.cs
+++ Ghpr.MSTest/TrxReader.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml;
+using Ghpr.Core;
+using Ghpr.Core.Common;
 using Ghpr.Core.Interfaces;
 
 namespace Ghpr.MSTest
@@ -28,13 +32,56 @@ namespace Ghpr.MSTest
             return this;
         }
 
+        public string GetRunGuid()
+        {
+            var tr = _xml.GetElementsByTagName("TestRun")[0];
+            var id = tr.Attributes?["id"].Value ?? Guid.NewGuid().ToString();
+            return id;
+        }
+
         public List<ITestRun> GetTestRuns()
         {
             var testRuns = new List<ITestRun>();
 
+            var utrs = _xml.GetElementsByTagName("UnitTestResult");
+            
+            foreach (XmlNode utr in utrs)
+            {
+                var start = DateTime.Parse(utr.Attributes?["startTime"].Value);
+                var finish = DateTime.Parse(utr.Attributes?["endTime"].Value);
+                var testGuid = utr.Attributes?["id"].Value ?? Guid.NewGuid().ToString();
+                var testInfo = new ItemInfo
+                {
+                    Start = start,
+                    Finish = finish,
+                    Guid = Guid.Parse(testGuid)
+                };
+                var testName = utr.Attributes?["testName"].Value;
+                var testFullName = _xml.GetElementById(testGuid)?
+                    .GetElementsByTagName("TestMethod")[0]
+                    .Attributes?["className"]
+                    .Value
+                    .Split(',')[0] + "." + testName;
+                var result = utr.Attributes?["outcome"].Value;
+                var testRun = new TestRun
+                {
+                    TestInfo = testInfo,
+                    Name = testName,
+                    FullName = testFullName,
+                    Result = result
+                };
+                
+                testRuns.Add(testRun);
+            }
+
             return testRuns;
         }
 
-
+        public void GenerateReport(Reporter r)
+        {
+            var runGuid = GetRunGuid();
+            var testRuns = GetTestRuns();
+            r.GenerateFullReport(testRuns, runGuid);
+        }
     }
 }
