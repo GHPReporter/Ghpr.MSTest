9584fc04da22fd14fd7f84777741bf5e28061b5b
Evgeniy Kosjakov <42_@mail.ru>
2017-07-28 20:27:56 +0300

    core updated

--- Ghpr.MSTest/Program.cs
+++ Ghpr.MSTest/Program.cs
@@ -7,7 +7,10 @@ namespace Ghpr.MSTest
     {
         public static void Main(string[] args)
         {
-            if (!args.Any()) throw new Exception("No path specified!");
+            if (!args.Any())
+            {
+                throw new Exception("No path specified!");
+            }
             var path = args[0];
             ReportGenerator.GenerateReport(path);
         }
