d1bd85dab6ead7135caeb766cd83c61b59ad3ab3
Evgeniy Kosjakov <42_@mail.ru>
2017-12-26 21:18:49 +0300

    converted to library to use in common ghpr console application

---
+++ Ghpr.MSTest/Utils/GhprMSTestRunHelper.cs
@@ -0,0 +1,26 @@
+﻿// ReSharper disable InconsistentNaming
+using System;
+using Ghpr.Core;
+using Ghpr.Core.Enums;
+
+namespace Ghpr.MSTest.Utils
+{
+    public class GhprMSTestRunHelper
+    {
+        public static void CreateReportFromFile(string path)
+        {
+            var reporter = new Reporter(TestingFramework.MSTest);
+            try
+            {
+                var reader = new TrxReader(path);
+                var testRuns = reader.GetTestRuns();
+                reporter.GenerateFullReport(testRuns);
+            }
+            catch (Exception ex)
+            {
+                var log = new Core.Utils.Log(reporter.Settings.OutputPath);
+                log.Exception(ex, "Exception in CreateReportFromFile");
+            }
+        }
+    }
+}
