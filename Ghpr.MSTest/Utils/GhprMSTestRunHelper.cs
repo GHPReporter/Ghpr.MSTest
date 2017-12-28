1f1eb34edbbd95726e2c5d15dd16640483bfafc7
Evgeniy Kosjakov <42_@mail.ru>
2017-12-28 19:33:45 +0300

    new method added

--- Ghpr.MSTest/Utils/GhprMSTestRunHelper.cs
+++ Ghpr.MSTest/Utils/GhprMSTestRunHelper.cs
@@ -1,7 +1,9 @@
 ﻿// ReSharper disable InconsistentNaming
 using System;
+using System.Collections.Generic;
 using Ghpr.Core;
 using Ghpr.Core.Enums;
+using Ghpr.Core.Interfaces;
 
 namespace Ghpr.MSTest.Utils
 {
@@ -12,14 +14,30 @@ namespace Ghpr.MSTest.Utils
             var reporter = new Reporter(TestingFramework.MSTest);
             try
             {
-                var reader = new TrxReader(path);
-                var testRuns = reader.GetTestRuns();
+                var testRuns = GetTestRunsListFromFile(path);
                 reporter.GenerateFullReport(testRuns);
             }
             catch (Exception ex)
             {
                 var log = new Core.Utils.Log(reporter.Settings.OutputPath);
                 log.Exception(ex, "Exception in CreateReportFromFile");
+            }
+        }
+
+        public static List<ITestRun> GetTestRunsListFromFile(string path)
+        {
+            try
+            {
+                var reader = new TrxReader(path);
+                var testRuns = reader.GetTestRuns();
+                return testRuns;
+            }
+            catch (Exception ex)
+            {
+                var reporter = new Reporter(TestingFramework.MSTest);
+                var log = new Core.Utils.Log(reporter.Settings.OutputPath);
+                log.Exception(ex, "Exception in GetTestRunsListFromFile");
+                return null;
             }
         }
     }
